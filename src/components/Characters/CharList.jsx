9d7087d9cbd13a274495641afaa3e929d05d3485
Nicole Martin <n.martinpdx@gmail.com>
2021-11-09 15:47:53 -0800

    average sum of mass and height added

--- src/components/Characters/CharList.jsx
+++ src/components/Characters/CharList.jsx
@@ -6,23 +6,45 @@ import { useChars } from '../../hooks/charHooks';
 const CharList = () => {
   const { loading, chars } = useChars();
 
+  // Calculate the average Height using reduce!
+  const charHeight = chars.map((char) => {
+    return `${char.height}`;
+  });
+
+  const averageHeight = charHeight.reduce((average, height, index, array) => {
+    return average + height / array.length;
+  }, 0);
+
+  // Calculate the average Mass using reduce!
+  const charMass = chars.map((char) => {
+    return `${char.mass}`;
+  });
+
+  const averageMass = charMass.reduce((average, mass, index, array) => {
+    return average + mass / array.length;
+  }, 0);
+
   if (loading) {
     return <Spinner />;
   } else {
     return (
-      <ul>
-        {chars.map((char) => (
-          <li key={char.name}>
-            <CharItem
-              name={char.name}
-              gender={char.gender}
-              hair={char.hair}
-              mass={char.mass}
-              height={char.height}
-            />
-          </li>
-        ))}
-      </ul>
+      <>
+        <ul>
+          {chars.map((char) => (
+            <li key={char.name}>
+              <CharItem
+                name={char.name}
+                gender={char.gender}
+                hair={char.hair}
+                mass={char.mass}
+                height={char.height}
+              />
+            </li>
+          ))}
+        </ul>
+        <span>Average Height: {averageHeight} </span>
+        <span>Average Mass: {averageMass} </span>
+      </>
     );
   }
 };
---
+++ src/components/UI/Pagination.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Pagination = () => {
+  const [page, setPage] = useState(1);
+};
+
+export default Pagination;
